35dc294819439055b236a05440aa87fda87d573b
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-05-22 22:16:10 -0400

    finish Adding an Item exercise

--- src/ToDos.java
+++ src/ToDos.java
@@ -7,10 +7,13 @@ class ToDos {
     ArrayList<String> toDoList = new ArrayList<String>();
     String toDo1 = "Water plants";
     // Add more to-dos here:
-    
-    
+    String toDo2 = "Walk dog";
+    String toDo3 = "Practice Java";
+
     // Add to-dos to toDoList
-    
+    toDoList.add(toDo1);
+    toDoList.add(toDo2);
+    toDoList.add(toDo3);
     
     System.out.println(toDoList);
       
